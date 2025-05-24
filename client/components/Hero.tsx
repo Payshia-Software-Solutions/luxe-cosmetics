0a6a74228618b07f74fcb386cf264c5a6c47391b
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-05-24 17:25:30 +0530

    Update Hero.tsx

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { ArrowRight, ChevronLeft, ChevronRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 // Define the type for each slide
